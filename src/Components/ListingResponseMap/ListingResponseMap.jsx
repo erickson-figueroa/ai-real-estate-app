c85c1b5f87536bd0d8623d36bd9b9dd3d2b60920
indetrd <indetrd@gmail.com>
2024-07-31 02:38:03 +0000

    Custom mark icon added to the map

--- src/Components/ListingResponseMap/ListingResponseMap.jsx
+++ src/Components/ListingResponseMap/ListingResponseMap.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import locationIcon from '../../assets/location.png';
 
 const ListingResponseMap = ({ latitude, longitude, mapId, address, neighborhood, price }) => {
   useEffect(() => {
@@ -10,7 +11,16 @@ const ListingResponseMap = ({ latitude, longitude, mapId, address, neighborhood,
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    const marker = L.marker([latitude, longitude]).addTo(map);
+
+    const customIcon = L.icon({
+      iconUrl: locationIcon,
+      iconSize: [50, 50], // size of the icon
+      iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+      popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+
+    const marker = L.marker([latitude, longitude], { icon: customIcon }).addTo(map);
+    marker.bindPopup(`<b>${address}</b><br>${neighborhood}<br>Price: ${price}`).openPopup();
 
     marker.bindPopup(`
       <div>
